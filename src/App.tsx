86e1702fe07be3e770f8a956de8faf309190f3d7
loveCamilleTW <yoyopetergod@gmail.com>
2023-05-06 15:26:55 +0800

    Feat: implement login with GitHub

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { Button } from "@mui/material";
+
 function App() {
+  const handleClick = () => {
+    const clientId = "2686b078ae4341574f65";
+    const scope = "repo";
+
+    const url = "https://github.com/login/oauth/authorize";
+    const params = `client_id=${clientId}&scope=${scope}`;
+
+    window.location.assign(`${url}?${params}`);
+  };
+
   return (
     <>
       <h1>Vite + React</h1>
+      <Button onClick={handleClick}>Login with GitHub</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
